393e739c1d79b25e136f33f593002cbb167553bf
Rodolfo <rodolfo.saldanha@hotmail.com>
2016-11-30 15:32:19 -0200

    MyClient was updated with interface, but not calling functions. Function RetriveTime updated, but contains lexic errors.

--- AdderRemote.java
+++ AdderRemote.java
@@ -70,24 +70,53 @@ public class AdderRemote extends UnicastRemoteObject implements Adder{
     			Class.forName("org.postgresql.Driver");  
 			Connection con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/SistemasOperacionais","postgres","senha");   
 			List<String> posts = new ArrayList<String>;
-			PreparedStatement ps=con.prepareStatement("SELECT * FROM topico WHERE username=(?)");
+			
+			// procura todos os posts de um determinado usuario a partir do tempo especificado
+			PreparedStatement ps=con.prepareStatement("SELECT * FROM topico WHERE username=(?) AND sod=1");
 			ps.setString(1, nome);
 			ResultSet rs=ps.executeQuery(); 
 
 			if (rs.next()){
-				PreparedStatement ps1=con.prepareStatement("SELECT username,topico,texto FROM post WHERE time>=(?) AND topico='#sod'",(tempo,)) // procura todos os posts de um determinado usuario a partir do tempo especificado
+				PreparedStatement ps1=con.prepareStatement("SELECT username,topico,texto FROM post WHERE time>=(?) AND topico='#sod'")
+				ps1.setString(1, tempo);
+				ResultSet rs1=ps1.executeQuery();
+				while(rs1.next()){
+					posts.add("Username:" + rs1.getString("username") + " topico:" + rs1.getString("topico") + " postou: " + rs1.getString("texto")
+				}
+			}
 
-			results = cursor.fetchall()
-			for row in results:
-				posts += 'Username:' + row[0] + ' topico:' + row[1] + ' postou: ' + row[2] + '\n'
-		
-		cursor.execute("SELECT * FROM topico WHERE username=(?) AND cc=1", (nome,))
-		x =  cursor.fetchone()
-			}  
+			// procura todos os posts de um determinado usuario a partir do tempo especificado
+			PreparedStatement ps2=con.prepareStatement("SELECT * FROM topico WHERE username=(?) AND cc=1");
+			ps2.setString(1, nome);
+			ResultSet rs2=ps2.executeQuery(); 
+
+			if (rs2.next()){
+				PreparedStatement ps3=con.prepareStatement("SELECT username,topico,texto FROM post WHERE time>=(?) AND topico='#cc'")
+				ps3.setString(1, tempo);
+				ResultSet rs3=ps3.executeQuery();
+				while(rs3.next()){
+					posts.add("Username:" + rs3.getString("username") + " topico:" + rs3.getString("topico") + " postou: " + rs3.getString("texto")
+				}
+			}
+
+			// procura todos os posts de um determinado usuario a partir do tempo especificado
+			PreparedStatement ps4=con.prepareStatement("SELECT * FROM topico WHERE username=(?) AND cd=1");
+			ps4.setString(1, nome);
+			ResultSet r4s=ps4.executeQuery(); 
+
+			if (rs4.next()){
+				PreparedStatement ps5=con.prepareStatement("SELECT username,topico,texto FROM post WHERE time>=(?) AND topico='#cd'")
+				ps5.setString(1, tempo);
+				ResultSet rs5=ps5.executeQuery();
+				while(rs5.next()){
+					posts.add("Username:" + rs5.getString("username") + " topico:" + rs5.getString("topico") + " postou: " + rs5.getString("texto")
+				}
+			}
+
       			con.close();
       			return 1;
     		}catch(Exception e){
-    			posts.append("Erro: USERNAME inexistente ou DATA escrita de forma errada");
+    			posts.add("Erro: USERNAME inexistente ou DATA escrita de forma errada");
     		}
 		return posts; // retorna lista de posts encontrados
 	}
--- MyClient.java
+++ MyClient.java
@@ -1,16 +1,92 @@
-import java.rmi.*;  
-import java.rmi.server.*;  
-import java.sql.*;  
-import java.util.*; 
+import java.rmi.*;
+import java.util.Date;
+import java.util.Scanner;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.sql.Timestamp;
 
 public class MyClient{
-	
+
 	public static void main(String args[]){
+
 		try{
+
+			 //fazendo a conexão com o servidor
 			Adder stub=(Adder)Naming.lookup("rmi://localhost:5000/sonoo");
+
+			Scanner ler = new Scanner(System.in);
+			int opcao;
+			String name;
+			String topic; 
+			String userName;
+			String topico;
+			String texto;
+			String date;
+			Timestamp timestamp;
+			
+			while(true){
+
+				System.out.println("1 - post\n2 - follow\n3 - unsubscribe\n4 - retrievetime\n5 - retrievetopic\n0 - Sair\nOpcao:");
+				opcao = ler.nextInt();
+
+				if(opcao == 0)
+					break;
+
+				else if(opcao == 1){
+
+					System.out.print("Digite o nome do usuário: ");
+					name = ler.next();
+					userName = ("@"+"name"+"\n");
+					System.out.print("Digite o tópico em que quer participar: ");
+					topic = ler.next();
+					topico = ("#"+"topic");
+				}
+				else if(opcao == 2){
+
+					System.out.print("Postado por: ");
+					name = ler.next();
+					userName = ("@"+"name"+"\n");
+					System.out.print("Tópico em que fara o post: ");
+					topic = ler.next();
+					topico = ("#"+"topic");
+					System.out.print("Texto a ser postado: ");
+					texto = ler.next();
+				}
+				else if(opcao == 3){
+
+					System.out.print("Digite o nome do usuário: ");
+					name = ler.next();
+					userName = ("@"+"name"+"\n");
+					System.out.print("Digite o tópico em que deseja deixar de seguir: ");
+					topic = ler.next();
+					topico = ("#"+"topic");
+				}
+				else if(opcao == 4){
+
+					System.out.print("Digite o nome do usuário: ");
+					name = ler.next();
+					userName = ("@"+"name"+"\n");
+					//lendo a data com o timestamp
+					timestamp = new Timestamp(System.currentTimeMillis());
+					date = new SimpleDateFormat("dd/MM/yyyy").format(timestamp.getTime());
+					System.out.println(date);
+				}
+				else if(opcao == 5){
+
+					System.out.print("Digite o nome do usuário: ");
+					name = ler.next();
+					userName = ("@"+"name"+"\n");
+					System.out.print("Digite o tópico em que deseja deixar de seguir: ");
+					topic = ler.next();
+					topico = ("#"+"topic");
+					//lendo a data com o timestamp
+					timestamp = new Timestamp(System.currentTimeMillis());
+					date = new SimpleDateFormat("dd/MM/yyyy").format(timestamp.getTime());
+					System.out.println(date);
+				}
+			}
 			System.out.println(stub.add(34,4));
-			System.out.println(stub.sub(34,4));
-			System.out.println(stub.getCustomers());  
 
 		}catch(Exception e){System.out.println(e);}
 	}
